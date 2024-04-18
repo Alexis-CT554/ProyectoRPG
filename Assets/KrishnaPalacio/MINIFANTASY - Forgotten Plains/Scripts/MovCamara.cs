6a9edad65b8cdee401ee8a6402544848af43c438
gelogr <167271988+gelogr@users.noreply.github.com>
2024-04-17 22:28:00 -0600

    portal

--- Assets/KrishnaPalacio/MINIFANTASY - Forgotten Plains/Scripts/MovCamara.cs
+++ Assets/KrishnaPalacio/MINIFANTASY - Forgotten Plains/Scripts/MovCamara.cs
@@ -8,27 +8,50 @@ public class MovCamara : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D obj){  //PORTAL 1
         if(obj.gameObject.tag == "portal1"){
-            Vector3 posicioncamara = new Vector3(1, 1, 1);  //POSICION CAMERA
+            Vector3 posicioncamara = new Vector3(53.2f, 54.5f,-10);  //POSICION CAMERA
             camara.transform.position = posicioncamara;
-            Vector3 posicionPlayer = new Vector3(1, 1, 1); //POSICION PERSONAJE
+            Vector3 posicionPlayer = new Vector3(53.2f, 54.5f,-10); //POSICION PERSONAJE
+            this.transform.position = posicionPlayer;
+        }
+        if(obj.gameObject.tag == "portal4"){
+            Vector3 posicioncamara = new Vector3(26.6f, 21.3f,0);  //POSICION CAMERA
+            camara.transform.position = posicioncamara;
+            Vector3 posicionPlayer = new Vector3(26.6f, 21.3f,0); //POSICION PERSONAJE
+            this.transform.position = posicionPlayer;
+        
+
+        }
+        
+        if(obj.gameObject.tag == "portal2"){
+            Vector3 posicioncamara = new Vector3(154f, -6.1f, 0);  //POSICION CAMERA
+            camara.transform.position = posicioncamara;
+            Vector3 posicionPlayer = new Vector3(154f, -6.1f, 0); //POSICION PERSONAJE
             this.transform.position = posicionPlayer;
 
         }
-  if(obj.gameObject.tag == "portal2"){
-            Vector3 posicioncamara = new Vector3(1, 1, 1);  //POSICION CAMERA
+
+        if(obj.gameObject.tag == "portal5"){
+            Vector3 posicioncamara = new Vector3(58.7f, -0.3f, 0);  //POSICION CAMERA
             camara.transform.position = posicioncamara;
-            Vector3 posicionPlayer = new Vector3(1, 1, 1); //POSICION PERSONAJE
+            Vector3 posicionPlayer = new Vector3(58.7f, -0.3f, 0); //POSICION PERSONAJE
             this.transform.position = posicionPlayer;
 
         }
 
-          if(obj.gameObject.tag == "portal3"){
-            Vector3 posicioncamara = new Vector3(1, 1, 1);  //POSICION CAMERA
+        if(obj.gameObject.tag == "portal3"){
+            Vector3 posicioncamara = new Vector3(23.4f, -50.8f, 0);  //POSICION CAMERA
             camara.transform.position = posicioncamara;
-            Vector3 posicionPlayer = new Vector3(1, 1, 1); //POSICION PERSONAJE
+            Vector3 posicionPlayer = new Vector3(23.4f, -50.8f, 0); //POSICION PERSONAJE
             this.transform.position = posicionPlayer;
 
         }
+        if(obj.gameObject.tag == "portal6"){
+            Vector3 posicioncamara = new Vector3(16.6f, -16.7f, 0);  //POSICION CAMERA
+            camara.transform.position = posicioncamara;
+            Vector3 posicionPlayer = new Vector3(16.6f, -16.7f, 0); //POSICION PERSONAJE
+            this.transform.position = posicionPlayer;
 
+        }
+    
     }
 }
